fb3761978b54e6c112f13049b437836e718e2c27
malickndiaye85 <sn.malickndiaye@gmail.com>
2026-03-03 19:58:26 +0000

    Added env-config.js

---
+++ public/env-config.js
@@ -0,0 +1,11 @@
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyDPsWVCA_Czs64wxiBOqUCSWwbkLMPNjJo",
+  authDomain: "evenpasssenegal.firebaseapp.com",
+  databaseURL: "https://evenpasssenegal-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "evenpasssenegal",
+  storageBucket: "evenpasssenegal.firebasestorage.app",
+  messagingSenderId: "882782977052",
+  appId: "1:882782977052:web:1f2ea147010066017cf3d9"
+};
+
+console.log('[ENV] Firebase config loaded');
