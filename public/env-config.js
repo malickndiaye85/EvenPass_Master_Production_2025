1140b36ece6e5c72755874f2f11611e41be23e98
malickndiaye85 <sn.malickndiaye@gmail.com>
2026-03-10 17:11:30 +0000

    Updated quick-fix-898561.html

---
+++ public/env-config.js
@@ -0,0 +1,14 @@
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyDPsWVCA_Czs64wxiBOqUCSWwbkLMPNjJo",
+  authDomain: "evenpasssenegal.firebaseapp.com",
+  databaseURL: "https://evenpasssenegal-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "evenpasssenegal",
+  storageBucket: "evenpasssenegal.firebasestorage.app",
+  messagingSenderId: "882782977052",
+  appId: "1:882782977052:web:1f2ea147010066017cf3d9",
+  measurementId: "G-FVQTV8TMLJ"
+};
+
+window.FIREBASE_CONFIG = window.__FIREBASE_CONFIG__;
+
+console.log('[ENV] Firebase config loaded');
